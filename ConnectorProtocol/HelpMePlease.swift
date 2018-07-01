f1925039ef1b51fc8d2320c13e4d5c954a3feb3c
Berrie Kremers <berrie.kremers@xs4all.nl>
2018-07-01 20:42:41 +0200

    Update for iOS 12.

--- ConnectorProtocol/HelpMePlease.swift
+++ ConnectorProtocol/HelpMePlease.swift
@@ -69,12 +69,3 @@ extension MutableCollection {
         }
     }
 }
-
-public extension Sequence {
-    /// Returns an array with the contents of this sequence, shuffled.
-    public func shuffled() -> [Element] {
-        var result = Array(self)
-        result.shuffle()
-        return result
-    }
-}
