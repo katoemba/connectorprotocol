92fe308e23ead644380e29751b7bfb487a22105b
Berrie Kremers <berrie.kremers@icloud.com>
2025-12-11 21:22:08 +0100

    Use swift toolset 5.10.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ConnectorProtocol",
-    platforms: [.macOS(.v12), .iOS(.v14), .watchOS(.v10)],
+    platforms: [.macOS(.v14), .iOS(.v17), .watchOS(.v10), .tvOS(.v17)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "ConnectorProtocol", targets: ["ConnectorProtocol"])
