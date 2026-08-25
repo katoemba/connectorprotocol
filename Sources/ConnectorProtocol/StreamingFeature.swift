98a0c63a1cd00011e6d7824a7de21a0484d53b3d
Berrie Kremers <berrie.kremers@icloud.com>
2026-08-25 20:26:58 +0200

    Add streaming collection.

---
+++ Sources/ConnectorProtocol/StreamingFeature.swift
@@ -0,0 +1,21 @@
+//
+//  StreamingFeature.swift
+//  ConnectorProtocol
+//
+//  Created by Berrie Kremers on 25/08/2026.
+//
+
+import Foundation
+
+/// A struct defining a generic Streaming Collection object.
+/// This identifies streaming service specific collections, e.g. what friends listen to, recent, popular
+public struct StreamingCollection: Codable, Sendable {
+    /// A unique id for the streaming collection. Usage depends on library implementation.
+    public var id = ""
+    
+    /// The source of this streaming collection (i.e. the service like Spotify, LocalMusic etc).
+    public var source = SourceType.Unknown
+    
+    /// The name of the streaming collection.
+    public var name = ""
+}
